cf33024e42b30808e57e7abd63dd1165b92d44bf
varadhan <varadhan198@gmail.com>
2014-01-25 10:11:46 +0530

    initial changes

---
+++ resumemaker/copyright.js
@@ -0,0 +1,12 @@
+<!--
+copy="Copyright &copy; ";
+js=" webdevelopplus.com";
+today=new Date();
+y0=today.getFullYear();
+
+document.write("<style>.grey{FONT: 7.5pt verdana; COLOR: #666666; TEXT-DECORATION: none;}</style>");
+document.write("<TABLE cellSpacing=0 cellPadding=0 width=100% align=center border=0><TR>");
+document.write("<TD align=center class=grey><a class=grey target=_new href='http://www.webdevelopplus.com'>Privacy Policy</a> ~ ");
+document.write(copy+y0+js); 
+document.write("</TD></TR></TABLE>");
+ //-->
